b345c079682ae8022d08c55c80bfbf9f388b6eee
Baranyi Gábor <90318277+bgabesz1@users.noreply.github.com>
2021-10-24 20:05:04 +0200

    7.GYAK

---
+++ crc7fc_10.19/SaxCRC7FC/SaxCRC7FC/bin/SaxCRC7FC1020/SaxCRC7FC.java
@@ -0,0 +1,10 @@
+package SaxCRCZFC1020;
+
+public class SaxCRC7FC {
+    public SaxCRC7FC() {
+}
+
+    public static void main(String[] var0) {
+        throw new Error("Unresolved compilation problems: \n\tvoid is an invalid type for the variable startElement\n\tSyntax error on token \"(\", ; expected\n\tSyntax error on token \",\", ; expected\n\tSyntax error on token \",\", ; expected\n\tSyntax error on token \",\", ; expected\n\tAttributes cannot be resolved to a type\n\tSyntax error on token \")\", ; expected\n\tindent cannot be resolved to a variable\n\tThe method indent() is undefined for the type SaxOBYBZK\n\tvoid is an invalid type for the variable endElement\n\tSyntax error on token \"(\", ; expected\n\tDuplicate local variable uri\n\tSyntax error on token \",\", ; expected\n\tDuplicate local variable localName\n\tSyntax error on token \",\", ; expected\n\tDuplicate local variable qName\n\tSyntax error on token \")\", ; expected\n\tThe method indent() is undefined for the type SaxOBYBZK\n\tindent cannot be resolved to a variable\n\tvoid is an invalid type for the variable characters\n\tSyntax error on token \"(\", ; expected\n\tchard cannot be resolved to a type\n\tSyntax error on token \",\", ; expected\n\tSyntax error on token \",\", ; expected\n\tSyntax error on token \")\", ; expected\n\tindent cannot be resolved to a variable\n\tThe method indent() is undefined for the type SaxOBYBZK\n\tindent cannot be resolved to a variable\n\tSaxHandler cannot be resolved to a type\n\tSaxHandler cannot be resolved to a type\n");
+    }
+}
